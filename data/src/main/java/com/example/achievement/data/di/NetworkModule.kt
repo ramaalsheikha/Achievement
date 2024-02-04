d21e3c3d3f3cfeacba4cb5fa6d0aa7f6ac34b83a
rama <alsheikharama@gmail.com>
2024-02-04 21:40:22 +0300

    Creat NetworkModule as Singleton class using dependency injection

---
+++ data/src/main/java/com/example/achievement/data/di/NetworkModule.kt
@@ -0,0 +1,22 @@
+package com.example.achievement.data.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+    private const val baseUrl = "https://786b905e-735c-4be6-adfb-949d5dadee32.mock.pstmn.io/"
+
+    @Provides
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+}
